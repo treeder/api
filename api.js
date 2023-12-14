cc0005e7b6e828024f59bdd306a099da972a596d
Travis Reeder <treeder@gmail.com>
2023-12-14 11:14:01 -0800

    Update api.js

--- api.js
+++ api.js
@@ -8,8 +8,92 @@ apiInit({apiURL: '${d.apiURL}', getToken: () => auth.currentUser.getIdToken()})
 ```
 */
 
-var opts = {}
-var apiURL = ''
+export class API {
+
+    constructor(options = {}) {
+        this.options = options
+    }
+
+    /**
+    // fetch calls the API with all the details. 
+    // url is either a full URL or a path that will be appended to the apiURL option
+     */
+    async fetch(url, {
+        method = "GET",
+        body = {},
+        formData = null,
+        headers = {
+            "Content-Type": "application/json"
+        },
+        sessionCookie = "",
+        raw = false,
+    } = {},) {
+
+        method = method.toUpperCase()
+
+        if (!headers['Authorization']) {
+            // Cookie notes: cookies aren't passed in fetch by default: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+            // So we're doing some different various things here: explicity -> getToken() function -> 'session' cookie
+            if (sessionCookie && sessionCookie !== '') {
+                headers['Authorization'] = `Cookie ${sessionCookie}`
+            } else if (this.options.getToken) {
+                let token = await this.options.getToken()
+                headers['Authorization'] = "Bearer " + token
+            } else {
+                if (typeof window !== "undefined") {
+                    // running in browser
+                    let c = getCookie('session') || getCookie('session')
+                    if (c) {
+                        headers['Authorization'] = `Cookie ${c}`
+                    }
+                } else {
+                    // not running in browser
+                }
+            }
+        }
+
+        let apiURL = this.options.apiURL || ''
+        let data = {
+            method: method,
+            headers: headers
+        }
+        url = url.startsWith('http') ? url : apiURL + url
+        console.log("calling api:", url)
+        if (formData) {
+            data.body = formData
+            delete headers['Content-Type'] // see https://github.com/github/fetch/issues/505#issuecomment-293064470
+        } else if (!(method === 'GET' || method === 'HEAD')) {
+            data.body = JSON.stringify(body)
+        }
+        try {
+            let response = await fetch(url, data)
+            if (!response.ok) {
+                // console.log("RESPONSE STATUS:", response.status)
+                // console.log(response.headers.get('Content-Type'));
+                let ct = response.headers.get('Content-Type')
+                if (ct && ct.includes('application/json')) {
+                    let j = await response.json()
+                    // console.log("j:", j)
+                    throw new APIError(j.error.message, { status: response.status })
+                } else {
+                    throw new APIError(await response.text(), { status: response.status })
+                }
+            }
+            if (raw === true) {
+                return response
+            }
+            return await response.json()
+        } catch (e) {
+            // console.log("CAUGHT ERROR:", e)
+            throw e
+        }
+    }
+
+}
+
+// default Api instance
+const defaultAPI = new API()
+
 
 /**
  * // options:
@@ -17,8 +101,7 @@ var apiURL = ''
  * @param getToken = null // you can set this and the it MUST have a getToken() function that returns a promise. This will be passed in as a Bearer token.
  */
 export function apiInit(options = {}) {
-    opts = options
-    apiURL = opts.apiURL || ''
+    defaultAPI.options = options
 }
 
 export function getCookie(name) {
@@ -27,82 +110,7 @@ export function getCookie(name) {
     if (parts.length === 2) return parts.pop().split(';').shift();
 }
 
-/**
-// api calls the API with all the details. 
-// url is either a full URL or a path that will be appended to the apiURL option
- */
-export default async function api(url, {
-    method = "GET",
-    body = {},
-    formData = null,
-    headers = {
-        "Content-Type": "application/json"
-    },
-    sessionCookie = "",
-    raw = false,
-} = {},) {
-
-    method = method.toUpperCase()
-
-    if (!headers['Authorization']) {
-        // Cookie notes: cookies aren't passed in fetch by default: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
-        // So we're doing some different various things here: explicity -> getToken() function -> 'session' cookie
-        if (sessionCookie && sessionCookie !== '') {
-            headers['Authorization'] = `Cookie ${sessionCookie}`
-        } else if (opts.getToken) {
-            let token = await opts.getToken()
-            headers['Authorization'] = "Bearer " + token
-        } else {
-            if (typeof window !== "undefined") {
-                // running in browser
-                let c = getCookie('session') || getCookie('session')
-                if (c) {
-                    headers['Authorization'] = `Cookie ${c}`
-                }
-            } else {
-                // not running in browser
-            }
-        }
-    }
-
-    console.log("opts:", opts)
-    let apiURL = opts.apiURL || ''
-    let data = {
-        method: method,
-        headers: headers
-    }
-    url = url.startsWith('http') ? url : apiURL + url
-    console.log("calling api:", url)
-    if (formData) {
-        data.body = formData
-        delete headers['Content-Type'] // see https://github.com/github/fetch/issues/505#issuecomment-293064470
-    } else if (!(method === 'GET' || method === 'HEAD')) {
-        data.body = JSON.stringify(body)
-    }
-    try {
-        let response = await fetch(url, data)
-        if (!response.ok) {
-            // console.log("RESPONSE STATUS:", response.status)
-            // console.log(response.headers.get('Content-Type'));
-            let ct = response.headers.get('Content-Type')
-            if (ct && ct.includes('application/json')) {
-                let j = await response.json()
-                throw new ApiError(j.error.message, { status: response.status })
-            } else {
-                throw new ApiError(await response.text(), { status: response.status })
-            }
-        }
-        if (raw === true) {
-            return response
-        }
-        return await response.json()
-    } catch (e) {
-        // console.log("CAUGHT ERROR:", e)
-        throw e
-    }
-}
-
-class ApiError extends Error {
+class APIError extends Error {
     constructor(message, options = {}) { // matches standard Error and Response
         super(message, options)
         this.options = options
@@ -117,4 +125,7 @@ class ApiError extends Error {
     }
 }
 
-export { api, ApiError, apiURL }
+const api = defaultAPI.fetch.bind(defaultAPI)
+const apiURL = defaultAPI.apiURL
+
+export { api, APIError, apiURL }
